78ef325b0c6f120b2b86c6d64391df1dcc054b95
Adrian Miguel <adriandmiguel3826@gmail.com>
2026-09-03 11:17:24 +0800

    Activity 2.17 Box

---
+++ Box.java
@@ -0,0 +1,14 @@
+/*Adrian Miguel
+BSIT NETSEC 1-1*/
+public class Box {
+    public static void main(String[] args) {
+
+        System.out.println("*************         *");
+        System.out.println("*           *        * *");
+        System.out.println("*           *       *   *");
+        System.out.println("*           *      *     *");
+        System.out.println("*           *       *   *");
+        System.out.println("*           *        * *");
+        System.out.println("*************         *");
+    }
+}
