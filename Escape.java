32aa375a50eee0b26d7323c20cbac07ca50bf3a1
Adrian Miguel <adriandmiguel3826@gmail.com>
2026-09-03 10:52:12 +0800

    Activity 2.15 Escape

---
+++ Escape.java
@@ -0,0 +1,9 @@
+/*Adrian Miguel
+BSIT NETSEC*/
+public class Escape {
+    public static void main(String[] args) {
+
+        System.out.println("First Name\tLast Name\tStudent Number\tPhone Number");
+        System.out.println("Adrian\t\tMiguel\t\t26-204\t\t09352618521");
+    }
+}
